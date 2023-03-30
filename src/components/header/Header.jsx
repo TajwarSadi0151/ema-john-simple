115fb438611aeb961aa2d00210c85c79a1050a5e
TajwarSadi0151 <tajwaranjum015156552@gmail.com>
2023-03-31 05:52:29 +0600

    second-push

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is a header</h2>
+            <nav className='header'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/shop">shop</a>
+                    <a href="/order">order</a>
+                    <a href="/Inventory">Inventory</a>
+                    <a href="/login">login</a>
+                </div>
+            </nav>
         </div>
     );
 };
